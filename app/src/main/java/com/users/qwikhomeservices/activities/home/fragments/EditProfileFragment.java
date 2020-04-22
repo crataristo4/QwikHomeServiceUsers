ba1c83798207c75e51df444217cd10e7f6eebd23
Botnet <crataristo4@gmail.com>
2020-04-22 10:05:01 -0800

    fixed bugs

--- app/src/main/java/com/users/qwikhomeservices/activities/home/fragments/EditProfileFragment.java
+++ app/src/main/java/com/users/qwikhomeservices/activities/home/fragments/EditProfileFragment.java
@@ -2,6 +2,7 @@ package com.users.qwikhomeservices.activities.home.fragments;
 
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -19,7 +20,13 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 import com.users.qwikhomeservices.R;
 import com.users.qwikhomeservices.activities.home.MainActivity;
 import com.users.qwikhomeservices.activities.home.bottomsheets.EditItemBottomSheet;
@@ -28,13 +35,12 @@ import com.users.qwikhomeservices.databinding.FragmentEditProfileBinding;
 import com.users.qwikhomeservices.utils.DisplayViewUI;
 import com.users.qwikhomeservices.utils.MyConstants;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class EditProfileFragment extends Fragment {
 
     private FragmentEditProfileBinding fragmentEditProfileBinding;
@@ -43,7 +49,9 @@ public class EditProfileFragment extends Fragment {
     private long mLastClickTime = 0;
     private CircleImageView profilePhoto;
     private OnFragmentInteractionListener mListener;
-
+    private StorageReference mStorageReference;
+    private DatabaseReference usersDbAccountDbRef;
+    private String uid, getImageUri;
 
     public EditProfileFragment() {
         // Required empty public constructor
@@ -62,6 +70,14 @@ public class EditProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mStorageReference = FirebaseStorage.getInstance().getReference("photos");
+        uid = MainActivity.uid;
+        usersDbAccountDbRef = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("Users")
+                .child(uid);
+
 
         fragmentEditProfileBinding
                 .fabUploadPhoto.startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.fadein));
@@ -93,7 +109,7 @@ public class EditProfileFragment extends Fragment {
         fragmentEditProfileBinding.nameLayout.setOnClickListener(//open bottom sheet to edit name
                 this::onClick);
 
-        fragmentEditProfileBinding.aboutLayout.setOnClickListener(
+        fragmentEditProfileBinding.lastNameLayout.setOnClickListener(
                 //open bottom sheet to edit about
                 this::onClick);
 
@@ -104,8 +120,9 @@ public class EditProfileFragment extends Fragment {
 
     private void openGallery() {
         CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
                 .setAspectRatio(16, 16)
-                .start(Objects.requireNonNull(getActivity()));
+                .start(Objects.requireNonNull(getContext()), this);
     }
 
     private void onClick(View v) {
@@ -130,7 +147,7 @@ public class EditProfileFragment extends Fragment {
             }
 
 
-        } else if (v.getId() == R.id.aboutLayout) {
+        } else if (v.getId() == R.id.lastNameLayout) {
             String getLastName = String.valueOf(fragmentEditProfileBinding.txtLastName.getText());
             bundle.putString(MyConstants.LAST_NAME, getLastName);
             editItemBottomSheet.setArguments(bundle);
@@ -151,12 +168,12 @@ public class EditProfileFragment extends Fragment {
                 assert result != null;
                 uri = result.getUri();
 
-                /*Glide.with(getActivity())
+                Glide.with(Objects.requireNonNull(getActivity()))
                         .load(uri)
                         .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(profileImage);*/
+                        .into(profilePhoto);
 
-                // uploadFile();
+                uploadFile();
 
             } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                 // progressDialog.dismiss();
@@ -166,6 +183,71 @@ public class EditProfileFragment extends Fragment {
             }
         }
     }
+
+    private void uploadFile() {
+        if (uri != null) {
+            ProgressDialog progressDialog = DisplayViewUI.displayProgress(getActivity(), "updating profile picture please wait...");
+            progressDialog.show();
+
+            // final File thumb_imageFile = new File(Objects.requireNonNull(uri.getPath()));
+
+          /*  try {
+                Bitmap thumb_imageBitmap = new Compressor(Objects.requireNonNull(getActivity()))
+                        .setMaxHeight(130)
+                        .setMaxWidth(13)
+                        .setQuality(100)
+                        .compressToBitmap(thumb_imageFile);
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                thumb_imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
+
+            //  file path for the itemImage
+            final StorageReference fileReference = mStorageReference.child(uid + "." + uri.getLastPathSegment());
+
+            fileReference.putFile(uri).continueWithTask(task -> {
+                if (!task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    DisplayViewUI.displayToast(getActivity(), Objects.requireNonNull(task.getException()).getMessage());
+
+                }
+                return fileReference.getDownloadUrl();
+
+            }).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+
+                    Uri downLoadUri = task.getResult();
+                    assert downLoadUri != null;
+                    getImageUri = downLoadUri.toString();
+
+                    Map<String, Object> updatePhoto = new HashMap<>();
+                    updatePhoto.put("image", getImageUri);
+
+                    usersDbAccountDbRef.updateChildren(updatePhoto).addOnCompleteListener(task12 -> {
+                        if (task12.isSuccessful()) {
+                            progressDialog.dismiss();
+                            DisplayViewUI.displayToast(getActivity(), "Successful");
+                        } else {
+                            progressDialog.dismiss();
+                            DisplayViewUI.displayToast(getActivity(), Objects.requireNonNull(task12.getException()).getMessage());
+
+                        }
+                    });
+
+
+                } else {
+                    progressDialog.dismiss();
+                    DisplayViewUI.displayToast(getActivity(), Objects.requireNonNull(task.getException()).getMessage());
+
+                }
+
+            });
+        }
+    }
+
 
 
     @Override
